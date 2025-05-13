ac5d0a50bd2f7c88babdb0b8e70081d030cd8a0c
Leonardo_S <leonardo.sanchez.t@utec.edu.pe>
2025-05-13 16:58:37 -0500

    añadiendo carpeta listener

--- src/main/java/com/example/eventoss/listener/AuditLogListener.java
+++ src/main/java/com/example/eventoss/listener/AuditLogListener.java
@@ -1,4 +1,4 @@
-package com.example.eventoss;
+package com.example.eventoss.listener;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
