ebd5ee82113ee688f1aca35a77b0f3f3c1440f1f
Joel Cayllahua <joel.game100xd@gmail.com>
2025-05-13 16:47:49 -0500

    incio

---
+++ src/main/java/com/example/eventoss/EventossApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventossApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventoss/EventossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
